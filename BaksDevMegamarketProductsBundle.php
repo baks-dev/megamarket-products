e47cb9c25c03117d6ee130274989aa26070a6a6a
kingstar <admin@baks.dev>
2024-06-10 02:30:02 +0300

    Baks Development

--- BaksDevMegamarketProductsBundle.php
+++ BaksDevMegamarketProductsBundle.php
@@ -35,4 +35,19 @@ class BaksDevMegamarketProductsBundle extends AbstractBundle
     public const NAMESPACE = __NAMESPACE__.'\\';
 
     public const PATH = __DIR__.DIRECTORY_SEPARATOR;
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $services = $container->services()
+            ->defaults()
+            ->autowire()
+            ->autoconfigure();
+
+        $services->load(self::NAMESPACE, self::PATH)
+            ->exclude([
+                self::PATH.'{Entity,Resources,Type}',
+                self::PATH.'**/*Message.php',
+                self::PATH.'**/*DTO.php',
+            ]);
+    }
 }
