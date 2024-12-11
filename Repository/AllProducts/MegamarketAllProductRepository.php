21fa6a65be4d53a51d84ac66d8d30cbfef455d53
kingstar <admin@baks.dev>
2024-12-11 13:21:44 +0300

    Baks Development

--- Repository/AllProducts/MegamarketAllProductRepository.php
+++ Repository/AllProducts/MegamarketAllProductRepository.php
@@ -452,8 +452,19 @@ final class MegamarketAllProductRepository implements MegamarketAllProductInterf
 			   
 			   ELSE NULL
 			END AS product_price
-		'
-        );
+		');
+
+        /* Предыдущая стоимость продукта */
+
+        $dbal->addSelect("
+			COALESCE(
+                NULLIF(product_modification_price.old, 0),
+                NULLIF(product_variation_price.old, 0),
+                NULLIF(product_offer_price.old, 0),
+                NULLIF(product_price.old, 0),
+                0
+            ) AS product_old_price
+		");
 
         /** Валюта продукта */
         $dbal->addSelect(
