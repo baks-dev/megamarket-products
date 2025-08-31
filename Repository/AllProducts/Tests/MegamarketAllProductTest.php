e7853b6f8ff1fccbd79f1aa92ea1e7e6579b8e9d
MichelAngelo <baksdevelopmen@gmail.com>
2025-09-01 00:15:49 +0300

    BaksDevelopment

--- Repository/AllProducts/Tests/MegamarketAllProductTest.php
+++ Repository/AllProducts/Tests/MegamarketAllProductTest.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,17 +26,15 @@ declare(strict_types=1);
 namespace BaksDev\Megamarket\Products\Repository\AllProducts\Tests;
 
 use BaksDev\Megamarket\Products\Repository\AllProducts\MegamarketAllProductInterface;
-
 use BaksDev\Products\Product\Type\Offers\Id\ProductOfferUid;
 use BaksDev\Products\Product\Type\Offers\Variation\Id\ProductVariationUid;
 use BaksDev\Products\Product\Type\Offers\Variation\Modification\Id\ProductModificationUid;
+use PHPUnit\Framework\Attributes\Group;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\DependencyInjection\Attribute\When;
 
-/**
- * @group megamarket-products-price
- */
 #[When(env: 'test')]
+#[Group('megamarket-products')]
 class MegamarketAllProductTest extends KernelTestCase
 {
     public function testUseCase(): void
